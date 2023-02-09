5fd617ae89918f1279f9fbe598827eceae0a5385
Matthew Bradbury <bradbm1@mcmaster.ca>
2023-02-09 18:56:15 -0500

    deleted polygon class

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/MeshADT.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/MeshADT.java
@@ -1,5 +1,6 @@
 package ca.mcmaster.cas.se2aa4.a2.generator;
-import ca.mcmaster.cas.se2aa4.a2.io.Structs;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Polygon;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,6 +9,7 @@ public class MeshADT {
 
     public void createPolygons(){
         //fill polygon arraylist with consecutive segments
+        PolygonList.add(Polygon.newBuilder().setSegmentIdxs(0, 0).setSegmentIdxs(1, 1).build());
     }
 
 }
