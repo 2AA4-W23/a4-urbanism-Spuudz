c0c16ef299cf02aedc8de15463b9240e619a7ce7
Matthew Bradbury <bradbm1@mcmaster.ca>
2023-02-09 13:50:06 -0500

    created meshADT and Polygon class

---
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/MeshADT.java
@@ -0,0 +1,14 @@
+package ca.mcmaster.cas.se2aa4.a2.generator;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MeshADT {
+
+
+
+    public void createPolygons(){
+        //fill polygon arraylist with consecutive segments
+    }
+
+}
---
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/Polygon.java
@@ -0,0 +1,10 @@
+package ca.mcmaster.cas.se2aa4.a2.generator;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Polygon {
+    public List<Structs.Segment> PolygonSegments = new ArrayList<Structs.Segment>(); 
+    public double centroid;
+
+}
