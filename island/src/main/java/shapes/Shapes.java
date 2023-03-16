226eb966d8d141d40abdda0e528f0d0a8aae552a
Dylan Garner <dylan.garner@gmail.com>
2023-03-16 14:39:08 -0400

    created IslandADT to mesh

--- island/src/main/java/shapes/Shapes.java
+++ island/src/main/java/shapes/Shapes.java
@@ -1,10 +1,10 @@
 package shapes;
-import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
+import IslandADT.*;
 import org.locationtech.jts.geom.Geometry;
 
 
 public interface Shapes {
 
-    public Mesh identifyLand(Mesh currentMesh, double centerX, double centerY,double width, double height);
+    public Island identifyLand(Island currentIsland, double centerX, double centerY,double width, double height);
 
 }
