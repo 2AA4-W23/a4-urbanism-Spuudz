5b9c7883b3988623d9b591cf1379ea59a6e7bf4e
Matthew Bradbury <bradbm1@mcmaster.ca>
2023-03-23 15:06:55 -0400

    Restructured shape and deleted beaches

--- island/src/main/java/shapes/Crescent.java
+++ island/src/main/java/shapes/Crescent.java
@@ -1,7 +1,6 @@
 package shapes;
 import Tiles.TileTypeChoose;
 import Tiles.TileType;
-import java.util.List;
 import java.util.Map;
 import IslandADT.*;
 
@@ -10,9 +9,6 @@ public class Crescent implements Shapes {
     public Crescent(Map<String, String> params){}
 
     public Island identifyLand(Island currentIsland, double centerX, double centerY,double width, double height){
-        
-        TileTypeChoose chooseTile = new TileTypeChoose();
-        String tile = "";
         double radius;
         Island clone = new Island();
         clone.register(currentIsland.getTileList(), currentIsland.getVerticesList(),currentIsland.getEdgesList());
@@ -24,46 +20,34 @@ public class Crescent implements Shapes {
         }
 
         for(Tile t : currentIsland.getTileList()){
-            String color = "";
             double x = currentIsland.getVertices(t.getCentroidIdx()).getX();
             double y = currentIsland.getVertices(t.getCentroidIdx()).getY();
             double distance = Math.sqrt(Math.pow(x-centerX,2) + Math.pow(y-centerY,2));
             double crescentDistance = Math.sqrt(Math.pow(x-(centerX-(radius*0.8)),2) + Math.pow(y-centerY,2));
-            if(distance>=radius){
-                color = chooseTile.getColor(TileType.Ocean);
-                tile = chooseTile.getTile(TileType.Ocean);
-            }else if(distance<radius){
-                if(crescentDistance > radius){
-                    Boolean oceanNeighbor = false;
-                    List <Integer> neighbors = t.getNeighborsIdxList();
-                    for(int neighbor : neighbors){
-                        Tile tempNeighbor = currentIsland.getTiles(neighbor);
-                        int tempCentroid = tempNeighbor.getCentroidIdx();
-                        double tempX = currentIsland.getVertices(tempCentroid).getX();
-                        double tempY = currentIsland.getVertices(tempCentroid).getY();
-                        double tempDistance = Math.sqrt(Math.pow(tempX-centerX,2) + Math.pow(tempY-centerY,2));
-                        double tempCrescentDistance = Math.sqrt(Math.pow(tempX-(centerX-radius*0.8),2) + Math.pow(tempY-centerY,2));
-                        if(tempDistance > radius || tempCrescentDistance <= radius){
-                            oceanNeighbor = true;
-                        }
-                    }
-                    if(oceanNeighbor){
-                        color = chooseTile.getColor(TileType.Beach);
-                        tile = chooseTile.getTile(TileType.Beach);
-                    }else{
-                        color = chooseTile.getColor(TileType.Forest);
-                        tile = chooseTile.getTile(TileType.Forest);
-                    }
-                }
-                else {
-                    color = chooseTile.getColor(TileType.Ocean);
-                    tile = chooseTile.getTile(TileType.Ocean);
-                }
-            }
-            t.setProperty("tile_type",tile);
-            t.setProperty("rgb_color",color);
+            t = assignType(t, distance, crescentDistance, radius);
         }
 
         return clone;    
+    }
+    public Tile assignType(Tile t, double distance, double crescentDistance, double radius){
+        String color = "";
+        TileTypeChoose chooseTile = new TileTypeChoose();
+        String tile = "";
+        if(distance>=radius){
+            color = chooseTile.getColor(TileType.Ocean);
+            tile = chooseTile.getTile(TileType.Ocean);
+        }else if(distance<radius){
+            if(crescentDistance > radius){
+                    color = chooseTile.getColor(TileType.Forest);
+                    tile = chooseTile.getTile(TileType.Forest);
+            }
+            else {
+                color = chooseTile.getColor(TileType.Ocean);
+                tile = chooseTile.getTile(TileType.Ocean);
+            }
+        }
+        t.setProperty("tile_type",tile);
+        t.setProperty("rgb_color",color);
+        return t;
     }
 }
