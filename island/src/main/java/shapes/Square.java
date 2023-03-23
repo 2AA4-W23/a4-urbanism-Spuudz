4d52c76544a1044a3b792035d05381a7798f9651
Harman-Bassi <harmanbassi29@gmail.com>
2023-03-22 20:28:12 -0400

    Updated square shape F09

--- island/src/main/java/shapes/Square.java
+++ island/src/main/java/shapes/Square.java
@@ -7,26 +7,19 @@ import Tiles.TileTypeChoose;
 import Tiles.TileType;
 import java.util.List;
 import java.util.Map;
-import IslandADT.Island;
-
-import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
-import ca.mcmaster.cas.se2aa4.a2.io.Structs;
-import ca.mcmaster.cas.se2aa4.a2.io.Structs.Polygon;
-import configuration.Configuration;
-import ca.mcmaster.cas.se2aa4.a2.io.MeshFactory;
+import IslandADT.*;
 
 public class Square implements Shapes {
 
     public Square(Map<String, String> params){}
 
-    public Island identifyLand(Island Island, double centerX, double centerY,double width, double height){
+    public Island identifyLand(Island currentIsland, double centerX, double centerY,double width, double height){
         
-        /*TileTypeChoose chooseTile = new TileTypeChoose();
+        TileTypeChoose chooseTile = new TileTypeChoose();
         String tile = "";
         double squareWidth;
-        Structs.Mesh.Builder clone = Structs.Mesh.newBuilder();
-        clone.addAllVertices(currentMesh.getVerticesList());
-        clone.addAllSegments(currentMesh.getSegmentsList());
+        Island clone = new Island();
+        clone.register(currentIsland.getTileList(), currentIsland.getVerticesList(),currentIsland.getEdgesList());
 
         if(width>height){
             squareWidth = width/3.14; 
@@ -38,55 +31,23 @@ public class Square implements Shapes {
         double yTop = centerY - squareWidth;
         double yBottom = centerY + squareWidth;
 
-        for(Structs.Polygon p : currentMesh.getPolygonsList()){
+        for(Tile t : currentIsland.getTileList()){
             String color = "";
-            double x = currentMesh.getVertices(p.getCentroidIdx()).getX();
-            double y = currentMesh.getVertices(p.getCentroidIdx()).getY();
+            double x = currentIsland.getVertices(t.getCentroidIdx()).getX();
+            double y = currentIsland.getVertices(t.getCentroidIdx()).getY();
 
             if((xLeft < x && xRight > x) && (yTop < y && yBottom > y)){
-                Boolean oceanNeighbor = false;
-                List <Integer> neighbors = p.getNeighborIdxsList();
-                for(int neighbor : neighbors){
-                    Polygon tempNeighbor = currentMesh.getPolygons(neighbor);
-                    int tempCentroid = tempNeighbor.getCentroidIdx();
-                    double tempX = currentMesh.getVertices(tempCentroid).getX();
-                    double tempY = currentMesh.getVertices(tempCentroid).getY();
-                    if((xLeft > tempX || xRight < tempX) || (yTop > tempY || yBottom < tempY)){
-                        oceanNeighbor = true;
-                    }
-                }
-                if(oceanNeighbor){
-                    color = chooseTile.getColor(TileType.Beach);
-                    tile = chooseTile.getTile(TileType.Beach);
-                }else{
-                    color = chooseTile.getColor(TileType.Forest);
-                    tile = chooseTile.getTile(TileType.Forest);
-                }
+                color = chooseTile.getColor(TileType.Forest);
+                tile = chooseTile.getTile(TileType.Forest);
                      
             }else{
                 color = chooseTile.getColor(TileType.Ocean);
                 tile = chooseTile.getTile(TileType.Ocean);
             }
 
-            Structs.Polygon.Builder pc = Structs.Polygon.newBuilder(p);
-
-                Structs.Property tt = Structs.Property.newBuilder()
-                        .setKey("tile_type")
-                        .setValue(tile)
-                        .build();
-                pc.addProperties(tt);
-
-                
-                Structs.Property pr = Structs.Property.newBuilder()
-                        .setKey("rgb_color")
-                        .setValue(color)
-                        .build();
-                pc.addProperties(pr);
-                clone.addPolygons(pc);
-        }
-
-        return clone.build();    
-        */
-        return Island;
+            t.setProperty("tile_type",tile);
+            t.setProperty("rgb_color",color);
+        }   
+        return clone;
     }
 }
