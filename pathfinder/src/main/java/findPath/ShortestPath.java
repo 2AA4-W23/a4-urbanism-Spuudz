4ca0b25eb4cd69a2929e6eb186d18ef2912679a2
Matthew Bradbury <bradbm1@mcmaster.ca>
2023-04-12 14:12:46 -0400

    removed unnecessary function

--- pathfinder/src/main/java/findPath/ShortestPath.java
+++ pathfinder/src/main/java/findPath/ShortestPath.java
@@ -13,23 +13,6 @@ public class ShortestPath implements FindPath{
         return Math.sqrt(Math.pow(n1.getX()-n2.getX(),2) + Math.pow(n1.getY()-n2.getY(),2));
     }
 
-    public void assignWeight(Graph G){
-        double distance;
-        for(Edge e : G.getEdgesList()){
-            int n1Idx = e.V1IDX();
-            int n2Idx = e.V2IDX();
-
-            double n1X = G.getNodeList().get(n1Idx).getX();
-            double n2X = G.getNodeList().get(n2Idx).getX();
-            double n1Y = G.getNodeList().get(n1Idx).getY();
-            double n2Y = G.getNodeList().get(n2Idx).getY();
-
-            distance = Math.sqrt(Math.pow((n1X - n2X),2) + Math.pow((n1Y - n2Y),2));
-
-            e.setProperty("weight", Double.toString(distance));
-        }
-    }
-
     public List<Integer> shortestPath(Graph G, Node n1, Node n2){
         ArrayList<Node> path = new ArrayList<Node>();
         ArrayList<Double> cost = new ArrayList<Double>();
